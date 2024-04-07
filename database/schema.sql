53e19c88ad411b60576130583dd9f14544a2bef8
53kyle <124002848+53kyle@users.noreply.github.com>
2024-04-07 00:57:17 -0700

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -9,12 +9,12 @@ DROP TABLE IF EXISTS cs470_Employee_Punchin;
 
 
 CREATE TABLE cs470_Employee (
-    id varchar(8) UNIQUE,
+    employee_id int(11) UNIQUE,
     first_name varchar(20) NOT NULL,
     last_name varchar(20) NOT NULL,
     permission bool,
     password_hash varchar(100) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (employee_id)
 );
 
 
@@ -33,29 +33,29 @@ CREATE TABLE cs470_Shift (
 
 
 CREATE TABLE cs470_Employee_Availability (
-    id varchar(8),
+    employee_id int(11),
     day_of_week varchar(10),
     start_time time,
     end_time time,
-    FOREIGN KEY (id) REFERENCES cs470_Employee(id) ON DELETE SET NULL
+    FOREIGN KEY (employee_id) REFERENCES cs470_Employee(employee_id) ON DELETE SET NULL
 );
 
 CREATE TABLE cs470_Employee_Timeoff (
-    id varchar(8),
+    employee_id int(11),
     start_time datetime,
     end_time datetime,
-    FOREIGN KEY (id) REFERENCES cs470_Employee(id) ON DELETE SET NULL
+    FOREIGN KEY (employee_id) REFERENCES cs470_Employee(id) ON DELETE SET NULL
 );
 
 
 CREATE TABLE cs470_Employee_Trained (
-    id varchar(8),
+    employee_id int(11),
     trained varchar(20),
-    FOREIGN KEY (id) REFERENCES cs470_Employee(id) ON DELETE SET NULL
+    FOREIGN KEY (employee_id) REFERENCES cs470_Employee(id) ON DELETE SET NULL
 );
        
 CREATE TABLE cs470_Employee_Punchin (
-    id varchar(8),
+    employee_id int(11),
     punchin datetime,
-    FOREIGN KEY (id) REFERENCES cs470_Employee(id) ON DELETE SET NULL
+    FOREIGN KEY (employee_id) REFERENCES cs470_Employee(id) ON DELETE SET NULL
 );
